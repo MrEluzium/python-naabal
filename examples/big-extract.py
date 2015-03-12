a4dafab49af1e2616c2fc3e98fe1153582690a58
aheadley <aheadley@waysaboutstuff.com>
2015-03-12 16:06:07 -0400

    Add .big file example scripts

---
+++ examples/big-extract.py
@@ -0,0 +1,73 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 Alex Headley <aheadley@waysaboutstuff.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+from naabal.formats.big.hw1 import HomeworldBigFile
+from naabal.formats.big.hwcata import CataclysmBigFile
+from naabal.formats.big.hw2 import Homeworld2BigFile
+from naabal.formats.big.hwrm import \
+    HomeworldClassicBigFile, Homeworld2ClassicBigFile, HomeworldRemasteredBigFile
+
+FORMAT_IDX = {
+    'hw1':      HomeworldBigFile,
+    'hw1c':     HomeworldClassicBigFile,
+    'hw2':      Homeworld2BigFile,
+    'hw2c':     Homeworld2ClassicBigFile,
+    'hwrm':     HomeworldRemasteredBigFile,
+}
+
+if __name__ == '__main__':
+    import argparse
+    import os
+    import os.path
+    import fnmatch
+
+    parser = argparse.ArgumentParser(prog='big-extract.py',
+        description='Extract contents of a .big file to a directory')
+    parser.add_argument('-f', '--format', choices=FORMAT_IDX, required=True)
+    parser.add_argument('-i', '--include-matching', nargs='+')
+    parser.add_argument('filename')
+    parser.add_argument('destination', default=os.getcwd(), nargs='?')
+    args = parser.parse_args()
+
+    if args.include_matching:
+        should_extract = lambda fn: any(fnmatch.fnmatch(fn, pattern) for pattern in args.include_matching)
+    else:
+        should_extract = lambda fn: True
+
+    bigfile = FORMAT_IDX[args.format](args.filename)
+    for entry in bigfile:
+        filename = bigfile.get_filename(entry)
+        full_filename = os.path.join(args.destination, filename)
+        if should_extract(filename):
+            try:
+                os.makedirs(os.path.dirname(full_filename))
+            except os.error:
+                # leaf dir already exists (probably)
+                pass
+            with open(full_filename, 'w') as data_file:
+                data_file.write(bigfile.get_data(entry))
+            print 'Extracted file: %s' % filename
+    bigfile.close()
---
+++ examples/big-ls.py
@@ -0,0 +1,56 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 Alex Headley <aheadley@waysaboutstuff.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+from naabal.formats.big.hw1 import HomeworldBigFile
+from naabal.formats.big.hwcata import CataclysmBigFile
+from naabal.formats.big.hw2 import Homeworld2BigFile
+from naabal.formats.big.hwrm import \
+    HomeworldClassicBigFile, Homeworld2ClassicBigFile, HomeworldRemasteredBigFile
+
+FORMAT_IDX = {
+    'hw1':      HomeworldBigFile,
+    'hw1c':     HomeworldClassicBigFile,
+    'hw2':      Homeworld2BigFile,
+    'hw2c':     Homeworld2ClassicBigFile,
+    'hwrm':     HomeworldRemasteredBigFile,
+}
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(prog='big-ls.py',
+        description='List contents of a .big file')
+    parser.add_argument('-f', '--format', choices=FORMAT_IDX, required=True)
+    parser.add_argument('filename')
+    args = parser.parse_args()
+    bigfile = FORMAT_IDX[args.format](args.filename)
+    for entry in bigfile:
+        print '{name_crc_start:0>8X}:{name_crc_end:0>8X} {state} {data_stored_size:>8} {data_real_size:>8} {timestamp} {filename}'.format(
+            filename=bigfile.get_filename(entry), state='c' if entry['data_compressed_flag'] else 'F',
+            name_crc_start=entry['name_crc_start'], name_crc_end=entry['name_crc_end'],
+            data_real_size=entry['data_real_size'], data_stored_size=entry['data_stored_size'],
+            timestamp=str(entry['timestamp']))
+    bigfile.close()
